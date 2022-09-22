8eca8883dfcfd270740230fde2d115373d992802
TongThanhTam_B1910137 <tamB1910137@student.ctu.edu.vn>
2022-09-22 11:44:36 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 let express = require("express");
 let cors = require("cors");
+let contactsRouter = require("./app/routes/contact.route");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use("/api/contacts", contactsRouter);
 
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application." });
